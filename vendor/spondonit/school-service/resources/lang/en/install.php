a8df1233821084dab6fbfd81d5604be71cd4e674
Edi Suwoto <edi.suwoto@gmail.com>
2021-10-09 09:15:51 +0700

    Create install.php

---
+++ vendor/spondonit/school-service/resources/lang/en/install.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    "welcome" => "Welcome",
+    "welcome_title" => "Welcome To Spondonit",
+"admin_setup" => "Admin Setup",
+"done_msg" => "Installation done. You can now login.",
+"email" => "Email",
+"password" => "Password",
+"password_confirmation" => "Confirm Password",
+'with_demo_data' => 'Install With Demo Data',
+"welcome_description" => "Thank you for choosing Infix School for online course management. Please follow the steps to complete Infix School installation!",
+);
